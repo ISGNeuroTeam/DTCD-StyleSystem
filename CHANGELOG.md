dfa6f40a2bf96ca30bb3e301dc43636b5b0cda78
sbelikov98 <sbelikov@isgneuro.com>
2021-12-10 14:31:09 +0300

    Added BaseTextarea custom component

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,6 +13,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - manifest file for dependencies
 - BaseButton custom component
 - Proxima Nova font
+- BaseTextarea custom component
 
 ### Changed
 
--- DTCD-StyleSystem/src/base-components/components.js
+++ DTCD-StyleSystem/src/base-components/components.js
@@ -1,5 +1,7 @@
 import BaseButton from './BaseButton/BaseButton';
+import BaseTextarea from './BaseTextarea/BaseTextarea';
 
 export default [
-  { name: 'base-button', baseClass: BaseButton }
+  { name: 'base-button', baseClass: BaseButton },
+  { name: 'base-textarea', baseClass: BaseTextarea },
 ];
