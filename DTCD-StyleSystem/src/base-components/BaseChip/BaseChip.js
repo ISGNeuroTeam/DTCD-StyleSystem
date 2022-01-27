2c7920f3bae5aaf43268fef04ad3d904f9c7d3f5
sbelikov98 <sbelikov@isgneuro.com>
2022-01-27 03:22:06 +0300

    Changed BaseChip custom component

--- DTCD-StyleSystem/src/base-components/BaseChip/BaseChip.js
+++ DTCD-StyleSystem/src/base-components/BaseChip/BaseChip.js
@@ -2,7 +2,6 @@ import html from './BaseChip.html';
 
 export default class BaseChip extends HTMLElement {
 
-  #chip;
   #icon;
   #iconClickHandler;
 
@@ -19,10 +18,8 @@ export default class BaseChip extends HTMLElement {
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.appendChild(template.content.cloneNode(true));
 
-    this.#iconClickHandler = () => this.dispatchEvent(new Event("remove")) || this.remove();
-
-    this.#chip = this.shadowRoot.querySelector('div');
     this.#icon = this.shadowRoot.querySelector('.icon');
+    this.#iconClickHandler = () => this.dispatchEvent(new Event("remove")) || this.remove();
 
     this.#icon.addEventListener('click', this.#iconClickHandler);
   }
