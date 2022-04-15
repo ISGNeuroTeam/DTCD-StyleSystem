3c39a8efc4e3cf96cdbb4f98d7b8a1635b379b7d
Alexandr-Bragin <abragin@isgneuro.com>
2022-04-15 18:34:03 +0900

    Added BaseSwitch documentation.

---
+++ DTCD-StyleSystem/src/stories/BaseSwitch.stories.js
@@ -0,0 +1,39 @@
+import BaseSwitch from '../base-components/BaseSwitch/BaseSwitch';
+
+import BaseSwitchDoc from './docs/BaseSwitchDoc.mdx';
+
+export default {
+  title: 'Example/BaseComponents/BaseSwitch',
+  parameters: {
+    docs: {
+      page: BaseSwitchDoc,
+    },
+  },
+};
+
+const NAME_COMPONENT = 'base-switch';
+
+window.customElements.define(NAME_COMPONENT, BaseSwitch);
+
+const Template = (args) => {
+  const {
+    disabled,
+    checked,
+    value,
+  } = args;
+
+  const switchInput = document.createElement(NAME_COMPONENT);
+
+  switchInput.checked = checked;
+  switchInput.disabled = disabled;
+  switchInput.value = value;
+
+  return switchInput;
+};
+
+export const DefaultRadio = Template.bind({});
+DefaultRadio.args = {
+  disabled: false,
+  checked: false,
+  value: '',
+};
