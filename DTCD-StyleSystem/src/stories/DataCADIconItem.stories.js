c055b22a5b2e877d7a3d1e46464a54f992e89f55
Alexandr-Bragin <abragin@isgneuro.com>
2022-04-26 17:40:42 +0900

    Added icon modifiers for rotation

--- DTCD-StyleSystem/src/stories/DataCADIconItem.stories.js
+++ DTCD-StyleSystem/src/stories/DataCADIconItem.stories.js
@@ -31,6 +31,29 @@ export default {
         '3xl',
       ],
     },
+    rotate: {
+      control: {
+        type: 'select',
+      },
+      options: [
+        '<default>',
+        'rotate_45',
+        'rotate_90',
+        'rotate_135',
+        'rotate_180',
+        'rotate_225',
+        'rotate_270',
+        'rotate_315',
+        'flip_horizontal',
+        'flip_vertical',
+        'flip_both',
+      ],
+    },
+    rotate_custom: {
+      control: {
+        type: 'number',
+      },
+    }
   },
   parameters: {
     docs: {
@@ -43,6 +66,8 @@ const Template = (args) => {
   const {
     size,
     name,
+    rotate,
+    rotate_custom,
   } = args;
 
   const icon = document.createElement('span');
@@ -56,11 +81,22 @@ const Template = (args) => {
     icon.classList.add(`size_${size}`);
   }
 
+  if (!rotate_custom) {
+    if (rotate && rotate != '<defalut>') {
+      icon.classList.add(rotate);
+    }
+  } else {
+    icon.classList.add('rotate_custom');
+    icon.setAttribute('style', `--font-icon-rotate-angle: ${rotate_custom}deg;`);
+  }
+
   return icon;
 };
 
 export const Icon = Template.bind({});
 Icon.args = {
-  size: '',
+  size: '<default>',
   name: ICON_NAMES[0],
+  rotate: '<default>',
+  rotate_custom: 0,
 };
